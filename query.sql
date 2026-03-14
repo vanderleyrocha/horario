de528c00622a2946a54eb5c96c7a8631732f5f4a
Vanderley Rocha <vanderley@bol.com.br>
2026-03-13 19:26:06 -0500

    ajustes 2026-03-13

---
+++ query.sql
@@ -0,0 +1,13 @@
+select professor_id,
+       disciplina_id,
+       turma_id,
+       aulas_semana,
+       tipo,
+       aulas_consecutivas,
+       max_aulas_dia,
+       min_intervalo_dias,
+       preferencia_periodo,
+       dias_preferidos,
+       tempos_preferidos
+  from aulas
+ where 1
